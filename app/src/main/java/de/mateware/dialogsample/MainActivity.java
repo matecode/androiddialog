c0263ef0b7c47208941aec52f6bbeebf5ba6bde2
Mate <mate@bunteban.de>
2016-04-26 22:42:22 +0200

    examples

--- app/src/main/java/de/mateware/dialogsample/MainActivity.java
+++ app/src/main/java/de/mateware/dialogsample/MainActivity.java
@@ -1,16 +1,12 @@
 package de.mateware.dialogsample;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 import de.mateware.dialog.Dialog;
+import de.mateware.dialog.DialogIndeterminateProgress;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,50 +16,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .show();
-            }
-        });
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Dialog().withMessage("Test")
-                            .withStyle(R.style.Dialog)
-                        .withTimer(10)
-                            .withPositiveButton()
-                            .showAllowStateLoss( getSupportFragmentManager(), "DIALOGTEST");
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void dialog(View view) {
+        new Dialog().withTitle("Dialog Title")
+                    .withMessage("This is a standard dialog.")
+                    .withStyle(R.style.Dialog)
+                    .withPositiveButton()
+                    .show(getSupportFragmentManager(), "DIALOG");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void indeterminateProgress(View view) {
+        new DialogIndeterminateProgress().withMessage("And the wheel goes round and round and round")
+                                         .withStyle(R.style.Dialog)
+                                         .withCancelable(false)
+                                         .withTimer(15)
+                                         .show(getSupportFragmentManager(), "PROGRESS_DIALOG");
     }
 }
